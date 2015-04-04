f7fcf239c9bcaacc78c29858f9cef366b86fd5ff
Thomas PIERRAIN <thomas@pierrain.net>
2015-04-04 19:07:04 +0200

    Restore solution for a new start

--- Diamonds/DiamondTests.cs
+++ Diamonds/DiamondTests.cs
@@ -1,9 +1,6 @@
 ﻿namespace Diamonds
 {
-    using System;
-
     using NFluent;
-
     using NUnit.Framework;
 
     [TestFixture]
@@ -15,66 +12,12 @@
             Check.That(DiamondPrinter.Print("A")).IsEqualTo("A");
         }
 
-        [Test]
-        public void ShouldPrintLetterB()
+        public static class DiamondPrinter
         {
-            Check.That(DiamondPrinter.Print("B")).IsEqualTo(
-@"-A
-B-B
--A");
-        }
-
-        [Test]
-        public void ShouldPrintLetterC()
-        {
-            Check.That(DiamondPrinter.Print("C")).IsEqualTo(
-@"--A
--B-B
-C---C
--B-B
---A");
-        }
-
-        //[Test]
-        public void ShouldPrintLetterE()
-        {
-            Check.That(DiamondPrinter.Print("E")).IsEqualTo(
-@"----A
----B-B
---C---C
--D-----D
-E-------E
--D-----D
---C---C
----B-B
-----A");
-        }
-    }
-
-    public static class DiamondPrinter
-    {
-        public static string Print(string letter)
-        {
-            if (letter == "A")
+            public static string Print(string letter)
             {
-                return "A";
+                return letter;
             }
-            if (letter == "B")
-            {
-                return "-" + "A" + Environment.NewLine + "B" + "-" + "B" + Environment.NewLine +"-" + "A";
-            }
-
-            if (letter == "C")
-            {
-                return "-" + "-" + "A" + Environment.NewLine + "-" + "B" + "-" + "B" + Environment.NewLine + "C" + "-" + "-" + "-" + "C" + Environment.NewLine + "-" + "B" + "-" + "B" + Environment.NewLine + "-" + "-" + "A";
-            }
-
-            //if (letter == "E")
-            //{
-            //    return "-" + "-" + "-" + "-" + "A" + Environment.NewLine + "-" + "-" + "-" + "B" + "-" + "B" + Environment.NewLine + "-" + "-" + "-" + "C" + "-" + "-" + "C" + Environment.NewLine + "-" + "-" + "-" + "B" + "-" + "B" + Environment.NewLine + "-" + "-" + "-" + "-" + "A";
-            //}
-
-            return letter;
         }
     }
 }
