f21326b173aca664cfdbbb742e190cf3318e715e
Thomas PIERRAIN <thomas@pierrain.net>
2015-04-18 10:15:15 +0200

    Remove the initial test

--- Diamonds/DiamondTests.cs
+++ Diamonds/DiamondTests.cs
@@ -6,18 +6,5 @@
     [TestFixture]
     public class DiamondTests
     {
-        [Test]
-        public void ShouldPrintLetterA()
-        {
-            Check.That(DiamondPrinter.Print("A")).IsEqualTo("A");
-        }
-
-        public static class DiamondPrinter
-        {
-            public static string Print(string letter)
-            {
-                return letter;
-            }
-        }
     }
 }
